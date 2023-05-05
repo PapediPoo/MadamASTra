3cbf195519d0ce71595a5fcc6d4c2c0d1c774f5d
PapediPoo <rschmidiger64@gmail.com>
2023-05-05 20:12:31 +0200

    changed argparser, added printing smt to file, add trying 2 words multiple times

--- MadamASTra/__main__.py
+++ MadamASTra/__main__.py
@@ -1,80 +1,37 @@
 """MadamASTra main
 
-This script is responsible for for generating and running the different subsystems of the program
+This script is responsible for for generating
+and running the different subsystems of the program
 
 """
 
 import argparse
-from random import randint
-from formula_generator import get_sat_z3_formulas, get_unsat_z3_formula, wrap_formula
-
-from z3_driver import Z3Driver
-from random_word import WordGenerator
-from c_printer import print_content, print_title, print_warning
-
-
-# bug: "livest", "call" for unsat with both solvers
-# also works for "X1245", "67XX"
-
-# bug: "hump", "owns" for unsat with z3str3
+from core_search import add_parser as add_search_parser
+from core_try import add_parser as add_try_parser
+from core_log import add_parser as add_log_parser
+from c_printer import print_title
 
 def main() -> None:
-    '''main function of the program. responsible for 
+    '''main function of the program. responsible for
     1. argparsing
-    2. running the wordgenerator, the SMT file generator and the SMT solver #runs times
+    2. running the wordgenerator,
+    the SMT file generator and the SMT solver #runs times
     3. reporting errors that z3 makes
     '''
     print_title("the clairvoyant will now tell you your fortune")
     # setup arg parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="store_true", help="makes the command line output more verbose")
-    parser.add_argument("-s", "--supress-warnings", help="currently does nothing")
-    parser.add_argument("-r", "--runs", type=int, default=5, help="number of times Z3 should be tested")
-    parser.add_argument("-wl", "--word-length", type=int, default=5, help="length of the generated words. Longer words => Z3 slower")
-    parser.add_argument("-wr", "--word-randomness", type=int, default=1, help="randomness of the length of words")
-    parser.add_argument("-t", "--timeout", type=int, default=30, help="Z3 timeout [seconds]. default: 30")
+    parser = argparse.ArgumentParser(
+        prog="MadamASTra", 
+        description="MadamASTra is a tool for finding bugs in Z3"
+        )
+    subparsers = parser.add_subparsers()
+    add_search_parser(subparsers.add_parser("search", help="search for bugs in Z3"))
+    add_try_parser(subparsers.add_parser("try", help="try the 2 provided words on Z3"))
+    add_log_parser(subparsers.add_parser("log", help="print the SMT file that would be generated for the 2 provided words"))
 
     args = parser.parse_args()
-
-    # setup word generator
-    wordgenerator = WordGenerator()
-
-    # setup Z3 driver
-    z3_driver = Z3Driver(args.verbose, args.timeout)
-
-    error_log = []
-    configs = [("sat", "seq", "unsat"),("unsat", "seq", "sat"),("sat", "z3str3", "unsat"),("unsat", "z3str3", "sat")]
-
-    for run in range(args.runs):
-        print_title(f"run {run}")
-        word1 = wordgenerator.generate(args.word_length + randint(-args.word_randomness, args.word_randomness))
-        word2 = wordgenerator.generate(args.word_length + randint(-args.word_randomness, args.word_randomness))
-        print_content(f"words: {word1}, {word2}")
-
-
-        mode_config, solver_config, wrong_response_config = configs[run % len(configs)]
-        print_content(f"mode: {mode_config}, solver: {solver_config}")
-        if mode_config == "sat":
-            generated_z3_formula, _ = get_sat_z3_formulas(word1, word2)
-        else:
-            generated_z3_formula = get_unsat_z3_formula(word1, word2)
-        full_input = wrap_formula(generated_z3_formula, solver_config)
-        z3_result = z3_driver.run(full_input)
-        print_content("Z3 Response: " + z3_result)
-        if wrong_response_config + "\n" == z3_result:
-            print_warning("Z3 Made a mistake!")
-            error_log.append((word1, word2, "solver: " + solver_config, "mode: " + mode_config))
-
-    print_title("done")
-    print_content("error log:")
-    print(error_log)
-    with open("bugs.txt", "a") as f:
-        for bug in error_log:
-            for e in bug:
-                f.write(e + ", ")
-            f.write("\n")
-    print_content("errors written to \"bugs.txt\"")
-
+    args.run_method(args)
+    
 
 if __name__ == "__main__":
     main()
---
+++ MadamASTra/core_try.py
@@ -0,0 +1,97 @@
+"""
+This module contains the function for trying two words on Z3,
+but with different solver configurations.
+Z3 is not entirely deterministic, so it is possible that Z3
+will give different results for the same input.
+This module tries to find such cases.
+"""
+
+import argparse
+from itertools import product
+from c_printer import print_content, print_title
+from z3_tester import Z3Tester
+
+
+def add_parser(parser: argparse.ArgumentParser) -> None:
+    '''
+    adds the arguments to the parser used for trying given words on Z3
+    '''
+    parser.set_defaults(run_mode="try")
+    parser.set_defaults(run_method=run)
+    parser.add_argument("word1", help="first word")
+    parser.add_argument("word2", help="second word")
+    parser.add_argument("-s", "--solver",
+                        choices=["seq", "z3str3", "both"],
+                        default="both",
+                        help="the solver to use. default: both")
+    parser.add_argument("-m", "--mode",
+                        choices=["sat", "unsat", "both"],
+                        default="both",
+                        help="the mode to use. default: both")
+    parser.add_argument("-t", "--timeout",
+                        type=int,
+                        default=30,
+                        help="Z3 timeout [seconds]. default: 30")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true",
+                        default=False,
+                        help="makes the command line output more verbose")
+    parser.add_argument("-r", "--retry",
+                        action="store_true",
+                        default=False,
+                        help="retry until Z3 makes a mistake")
+
+
+def run(args: argparse.Namespace) -> None:
+    '''tries word1 and word2 on Z3
+    args: the parsed arguments from the CLI
+    '''
+
+    # setup Z3 tester
+    z3_tester = Z3Tester(args.verbose, args.timeout)
+
+    print_content(f"words: {args.word1}, {args.word2}")
+
+    if args.mode == "both":
+        modes = ["sat", "unsat"]
+    elif args.mode == "sat":
+        modes = ["sat"]
+    elif args.mode == "unsat":
+        modes = ["unsat"]
+    else:
+        raise Exception("unknown mode. Z3Tester only supports sat and unsat")
+
+    if args.solver == "both":
+        solvers = ["seq", "z3str3"]
+    elif args.solver == "seq":
+        solvers = ["seq"]
+    elif args.solver == "z3str3":
+        solvers = ["z3str3"]
+    else:
+        raise Exception(
+            "unknown solver. Z3Tester only supports seq and z3str3"
+            )
+
+    configs = list(product(modes, solvers))
+
+    if args.retry:
+        # retry until Z3 makes a mistake. iteratively try all configurations
+        print_content("retrying until Z3 makes a mistake")
+        print_title("starting")
+        config_index = 0
+        while not z3_tester.test(
+                args.word1,
+                args.word2,
+                *configs[config_index]
+                ):
+            config_index = (config_index + 1) % len(configs)
+            print_title("retrying")
+    else:
+        # run Z3 once for each configuration
+        print_content("running Z3 once for each configuration")
+        print_title("starting")
+        for mode, solver in configs:
+            z3_tester.test(args.word1, args.word2, mode, solver)
+
+    print_title("done")
+    z3_tester.print_errors()
